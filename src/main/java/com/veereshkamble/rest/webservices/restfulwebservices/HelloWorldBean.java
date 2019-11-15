9f7da79a1cf2d9174ff5349cd376c3e479b2f454
veereshkamble <veereshkamble7@gmail.com>
2019-11-14 21:53:08 -0800

    Hello World Bean

---
+++ src/main/java/com/veereshkamble/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package com.veereshkamble.rest.webservices.restfulwebservices;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Veeresh Kamble
+ * Date: 2019-11-14
+ * Time: 21:50
+ */
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/veereshkamble/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/veereshkamble/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,7 +1,6 @@
 package com.veereshkamble.rest.webservices.restfulwebservices;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,8 +16,14 @@ public class HelloWorldController {
     // GET
     // URI - /hello-world
     // method - "Hello World"
-    @RequestMapping(method= RequestMethod.GET, path="/hello-world")
+    @GetMapping(path="/hello-world")
     public String HelloWorld() {
         return "Hello World";
+    }
+
+    //hello-world-bean
+    @GetMapping(path="/hello-world-bean")
+    public HelloWorldBean HelloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 }
