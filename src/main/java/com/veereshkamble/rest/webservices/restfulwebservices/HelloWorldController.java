c3e2fe64a657ef2ee796b8d0eabfe53056858755
veereshkamble <veereshkamble7@gmail.com>
2019-11-14 21:45:25 -0800

    Hello Wprld COntroller

--- src/main/java/com/veereshkamble/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/veereshkamble/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,11 +1,24 @@
 package com.veereshkamble.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Veeresh Kamble
  * Date: 2019-11-14
  * Time: 21:32
  */
+//Controller
+@RestController
 public class HelloWorldController {
-    
+
+    // GET
+    // URI - /hello-world
+    // method - "Hello World"
+    @RequestMapping(method= RequestMethod.GET, path="/hello-world")
+    public String HelloWorld() {
+        return "Hello World";
+    }
 }
