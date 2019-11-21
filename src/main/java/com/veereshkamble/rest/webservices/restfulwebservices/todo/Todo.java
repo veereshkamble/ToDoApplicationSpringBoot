1073d1f2f28f9e2be89e9435313aec538b50005a
veereshkamble <veereshkamble7@gmail.com>
2019-11-20 23:33:08 -0800

    Creating REST API for updating Todo - PUT Request Method

--- src/main/java/com/veereshkamble/rest/webservices/restfulwebservices/todo/Todo.java
+++ src/main/java/com/veereshkamble/rest/webservices/restfulwebservices/todo/Todo.java
@@ -17,6 +17,10 @@ public class Todo {
     private Date targetDate;
     private boolean isDone;
 
+    protected Todo() {
+        
+    }
+
     public Todo(long id, String username, String description, Date targetDate, boolean isDone) {
         this.id = id;
         this.username = username;
