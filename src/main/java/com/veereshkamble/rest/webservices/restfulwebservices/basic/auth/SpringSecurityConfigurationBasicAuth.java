df38ead25c0dc1599697971f3f6bb2abbc44703f
veereshkamble <veereshkamble7@gmail.com>
2019-12-05 22:16:43 -0800

    Configure Spring Security to disable CSRF and enable OPTION Requests

---
+++ src/main/java/com/veereshkamble/rest/webservices/restfulwebservices/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -0,0 +1,17 @@
+package com.veereshkamble.rest.webservices.restfulwebservices.basic.auth;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Veeresh Kamble
+ * Date: 2019-12-05
+ * Time: 22:13
+ */
+public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        
+    }
+}
